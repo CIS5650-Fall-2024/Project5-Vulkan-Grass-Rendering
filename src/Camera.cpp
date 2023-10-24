6ddd558b05d6f291d6a46022fc4201446511d49b
Aditya Gupta <adityag@gmail.com>
2023-10-23 21:28:38 -0400

    update camera pos and disalbe forecs and stuff

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -9,17 +9,17 @@
 #include "BufferUtils.h"
 
 Camera::Camera(Device* device, float aspectRatio) : device(device) {
-    r = 10.0f;
+    r = 20.0f;
     theta = 0.0f;
-    phi = 0.0f;
-    cameraBufferObject.viewMatrix = glm::lookAt(glm::vec3(0.0f, 1.0f, 10.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    phi = -35.0f;
     cameraBufferObject.projectionMatrix = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 100.0f);
     cameraBufferObject.projectionMatrix[1][1] *= -1; // y-coordinate is flipped
-    cameraBufferObject.invViewMatrix = glm::inverse(cameraBufferObject.viewMatrix);
 
     BufferUtils::CreateBuffer(device, sizeof(CameraBufferObject), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, buffer, bufferMemory);
     vkMapMemory(device->GetVkDevice(), bufferMemory, 0, sizeof(CameraBufferObject), 0, &mappedData);
     memcpy(mappedData, &cameraBufferObject, sizeof(CameraBufferObject));
+
+    UpdateOrbit(0, 0, 0);
 }
 
 VkBuffer Camera::GetBuffer() const {
